e1d6e661ada32b8fb1c6a34afcf05663904479fa
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-02 19:00:38 +0600

    do some changes in models.py

--- app/models.py
+++ app/models.py
@@ -3,35 +3,15 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator,MinValueValidator
 
 STATE_CHOICES = [
-    ('Andhra Pradesh', 'Andhra Pradesh'),
-    ('Arunachal Pradesh', 'Arunachal Pradesh'),
-    ('Assam', 'Assam'),
-    ('Bihar', 'Bihar'),
-    ('Chhattisgarh', 'Chhattisgarh'),
-    ('Goa', 'Goa'),
-    ('Gujarat', 'Gujarat'),
-    ('Haryana', 'Haryana'),
-    ('Himachal Pradesh', 'Himachal Pradesh'),
-    ('Jharkhand', 'Jharkhand'),
-    ('Karnataka', 'Karnataka'),
-    ('Kerala', 'Kerala'),
-    ('Madhya Pradesh', 'Madhya Pradesh'),
-    ('Maharashtra', 'Maharashtra'),
-    ('Manipur', 'Manipur'),
-    ('Meghalaya', 'Meghalaya'),
-    ('Mizoram', 'Mizoram'),
-    ('Nagaland', 'Nagaland'),
-    ('Odisha', 'Odisha'),
-    ('Punjab', 'Punjab'),
-    ('Rajasthan', 'Rajasthan'),
-    ('Sikkim', 'Sikkim'),
-    ('Tamil Nadu', 'Tamil Nadu'),
-    ('Telangana', 'Telangana'),
-    ('Tripura', 'Tripura'),
-    ('Uttar Pradesh', 'Uttar Pradesh'),
-    ('Uttarakhand', 'Uttarakhand'),
-    ('West Bengal', 'West Bengal'),
-]
+        ('DHAKA', 'Dhaka'),
+        ('CHITTAGONG', 'Chittagong'),
+        ('RAJSHAHI', 'Rajshahi'),
+        ('KHULNA', 'Khulna'),
+        ('BARISAL', 'Barisal'),
+        ('SYLHET', 'Sylhet'),
+        ('RANGPUR', 'Rangpur'),
+        ('MYMENSINGH', 'Mymensingh'),
+    ]
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
